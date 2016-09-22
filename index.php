572980313071d6a7c727295702440f8ddc43150a
Start Coding Student 1 <nicolas.clocksin@west-mec.org>
2016-09-22 14:18:27 -0700

    OOP Check #2

--- index.php
+++ index.php
@@ -11,16 +11,22 @@
 <?php
     $nick = new gameAccount('Nick', 'Password', 'nac1998@cox.net', '12312');
     $nick->set_name('Nick Clocksin');
-    $nick->set_password('Password');
-    $nick->set_account_number('12313');
     $nick->set_email('nac1998@cox.net');
     echo 'You name is ' . $nick->get_name();
     echo '<br />';
     echo 'Your email is ' . $nick->get_email();
-    $xbox = new xboxAccount($nick->get_name(), 'Halo4Ops', $nick->get_password(), $nick->get_account_number(), $nick->get_email());
+    $xbox = new xboxAccount($nick->get_name(), 'Halo4Ops', 'password', '1231231',$nick->get_email());
     $xbox->set_username('Halo4Ops');
     echo '<br />';
     echo $xbox->get_name() . ' your Xbox Gamertag is ' . $xbox->get_username();
+    $game = new Halo();
+    $game->name ='Halo';
+    $game->describe = 'A new Sci-fi game that takes you to a new destination, a Halo Ring.';
+    $game->rating = 'M';
+    $game->release = 'November 15, 2001';
+    echo '<br />';
+    echo $game->games();
+    echo steam_user::$min_password;
 ?>
 </body>
 </html>
